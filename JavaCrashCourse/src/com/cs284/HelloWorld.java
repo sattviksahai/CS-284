508063a7e9a21dd8135c87325b3a9021659d443e
sattviksahai <sattviksahai24@gmail.com>
2021-02-11 22:21:30 -0500

    added material for crash course 1

---
+++ JavaCrashCourse/src/com/cs284/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.cs284;
+
+import java.util.Date;
+
+public class HelloWorld {
+
+    public static Date now = new Date();
+
+    public static void main(String[] args) {
+        System.out.println("Hello again");
+        System.out.println(now);
+    }
+}
